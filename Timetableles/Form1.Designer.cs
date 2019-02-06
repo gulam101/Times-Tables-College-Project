160161555e7691401b91d75c15ceaab0a976379b
Gulam Qasim <Gulam1232018@outlook.com>
2019-02-06 21:06:26 +0000

    first commit

---
+++ Timetableles/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Timetableles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ExitCmd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(110, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(201, 45);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(294, 45);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(252, 185);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Click on any of the buttons!";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(386, 45);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(477, 45);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(477, 111);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "12";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(386, 111);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "11";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(294, 111);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "10";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(201, 111);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "9";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(110, 111);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 8;
+            this.button11.Text = "8";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(17, 111);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 7;
+            this.button12.Text = "7";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(230, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 24);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Times Table";
+            // 
+            // ExitCmd
+            // 
+            this.ExitCmd.Location = new System.Drawing.Point(29, 313);
+            this.ExitCmd.Name = "ExitCmd";
+            this.ExitCmd.Size = new System.Drawing.Size(75, 23);
+            this.ExitCmd.TabIndex = 14;
+            this.ExitCmd.Text = "Exit";
+            this.ExitCmd.UseVisualStyleBackColor = true;
+            this.ExitCmd.Click += new System.EventHandler(this.ExitCmd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 359);
+            this.Controls.Add(this.ExitCmd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Times Tables";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ExitCmd;
+    }
+}
+
---
+++ Timetableles/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timetableles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = ""; //Clears the label in the start
+
+            int counter; //Sets up the loop variable
+            int result;
+
+            for (counter = 1;
+                counter <= 12; counter++)
+            {
+                result = counter * 1;
+                label1.Text += counter.ToString() + "x1 = " +
+                    result.ToString() + "\n";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = ""; //Clears the label in the start
+
+            int counter; //Sets up the loop variable
+            int result;
+
+            for (counter = 1;
+                counter <= 12; counter++)
+            {
+                result = counter * 2;
+                label1.Text += counter.ToString() + "x2 = " +
+                    result.ToString() + "\n";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = ""; //Clears the label in the start
+
+            int counter; //Sets up the loop variable
+            int result;
+
+            for (counter = 1;
+                counter <= 12; counter++)
+            {
+                result = counter * 3;
+                label1.Text += counter.ToString() + "x3 = " +
+                    result.ToString() + "\n";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            {
+                label1.Text = ""; //Clears the label in the start
+
+                int counter; //Sets up the loop variable
+                int result;
+
+                for (counter = 1;
+                    counter <= 12; counter++)
+                {
+                    result = counter * 4;
+                    label1.Text += counter.ToString() + "x4 = " +
+                        result.ToString() + "\n";
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label1.Text = ""; //Clears the label in the start
+
+            int counter; //Sets up the loop variable
+            int result;
+
+            for (counter = 1;
+                counter <= 12; counter++)
+            {
+                result = counter * 5;
+                label1.Text += counter.ToString() + "x5 = " +
+                    result.ToString() + "\n";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label1.Text = ""; //Clears the label in the start
+
+            int counter; //Sets up the loop variable
+            int result;
+
+            for (counter = 1;
+                counter <= 12; counter++)
+            {
+                result = counter * 6;
+                label1.Text += counter.ToString() + "x6 = " +
+                    result.ToString() + "\n";
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            label1.Text = ""; //Clears the label in the start
+
+            int counter; //Sets up the loop variable
+            int result;
+
+            for (counter = 1;
+                counter <= 12; counter++)
+            {
+                result = counter * 7;
+                label1.Text += counter.ToString() + "x7 = " +
+                    result.ToString() + "\n";
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            label1.Text = ""; //Clears the label in the start
+
+            int counter; //Sets up the loop variable
+            int result;
+
+            for (counter = 1;
+                counter <= 12; counter++)
+            {
+                result = counter * 8;
+                label1.Text += counter.ToString() + "x8 = " +
+                    result.ToString() + "\n";
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            label1.Text = ""; //Clears the label in the start
+
+            int counter; //Sets up the loop variable
+            int result;
+
+            for (counter = 1;
+                counter <= 12; counter++)
+            {
+                result = counter * 9;
+                label1.Text += counter.ToString() + "x9 = " +
+                    result.ToString() + "\n";
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            label1.Text = ""; //Clears the label in the start
+
+            int counter; //Sets up the loop variable
+            int result;
+
+            for (counter = 1;
+                counter <= 12; counter++)
+            {
+                result = counter * 10;
+                label1.Text += counter.ToString() + "x10 = " +
+                    result.ToString() + "\n";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            label1.Text = ""; //Clears the label in the start
+
+            int counter; //Sets up the loop variable
+            int result;
+
+            for (counter = 1;
+                counter <= 12; counter++)
+            {
+                result = counter * 11;
+                label1.Text += counter.ToString() + "x11 = " +
+                    result.ToString() + "\n";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            label1.Text = ""; //Clears the label in the start
+
+            int counter; //Sets up the loop variable
+            int result;
+
+            for (counter = 1;
+                counter <= 12; counter++)
+            {
+                result = counter * 12;
+                label1.Text += counter.ToString() + "x12 = " +
+                    result.ToString() + "\n";
+            }
+        }
+
+        private void ExitCmd_Click(object sender, EventArgs e)
+        {
+           var ConfirmBox = MessageBox.Show("Are you sure you want to exit the application?", "Are you sure?", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+            if (ConfirmBox == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
